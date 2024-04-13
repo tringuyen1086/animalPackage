ee03b6635b967f517a8fd35e2cbcc0e8aed0baa6
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-04-13 00:47:39 -0700

    add dependencies from puppy and dog

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/tringuyen1086/animalPackage
 
 go 1.22.1
+
+require github.com/tringuyen1086/puppy v0.0.0-20240413073853-79cfb1f893ee
+
+require github.com/tringuyen1086/dog v0.0.0-20240413073617-67b48d64d4bb // indirect
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/tringuyen1086/puppy"
+)
 
 func main() {
-	fmt.Println("Golang!😍")
+	s1:= puppy.Bark()
+	s2:= puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3:= puppy.BigBark()
+	s4:= puppy.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)
+
+	// same as
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
 }
